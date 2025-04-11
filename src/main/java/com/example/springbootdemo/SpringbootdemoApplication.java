eb247e870273b0cfb0fd12f5663e81e247d9398a
Haritha Y <iasharitha@gmail.com>
2025-04-11 18:02:21 +0530

    migrated codebase to git repo

---
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+    public String hello() {
+      return String.format("<h1>Hello There!!! Welcome to my project</h1>");
+	}
+
+}
